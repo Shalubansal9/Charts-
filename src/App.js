e8dd14a49aefb574d7f073210b6cd1c83722ff60
Shalubansal9 <111283713+Shalubansal9@users.noreply.github.com>
2023-02-09 13:11:26 +0530

    chart

--- src/App.js
+++ src/App.js
@@ -1,24 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import LineChart from './components/LineChart';
+import BarChart from './components/BarChart';
+import PieChart from './components/PieChart';
+import {UserData} from './Data'
+import './App.css'
 
 function App() {
+  const initialData = {
+    labels: UserData.map((data)=>data.course),
+    datasets: [{
+      label: "Users with Adopted Technology",
+      data: UserData.map((data)=>data.user),
+      backgroundColor: ["#00ffff","#663399","#ff6347","#87ceeb","#fa8072"],
+    },],
+  };
+  const [userData, setUserData] = useState(initialData);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+    <div className='main'>
+      <div className='child' style={{width:500}}>
+        <p className='heading'>Line Chart</p>
+        <LineChart chartData={userData}/>
+      </div>
+      <div className='child' style={{width:500}}>
+        <p className='heading'>Bar Chart</p>
+        <BarChart chartData={userData}/>
+      </div>
+      <div className='child' style={{width:500}}>
+        <p className='heading'>Pie Chart</p>
+        <PieChart chartData={userData}/>
+      </div>
     </div>
+    </>
   );
 }
 
---
+++ src/Data.js
@@ -0,0 +1,27 @@
+export const UserData = [
+    {
+      id: 101,
+      course: "Vuejs",
+      user: 50500
+    },
+    {
+      id: 102,
+      course: "Reactjs",
+      user: 67344
+    },
+    {
+      id: 103,
+      course: "Angularjs",
+      user: 55180
+    },
+    {
+      id: 104,
+      course: "Javascript",
+      user: 88000
+    },
+    {
+      id: 105,
+      course: "App development",
+      user: 60000
+    }
+  ];
